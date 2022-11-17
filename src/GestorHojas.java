0a1fa1f4abcb361aa540c007ffef0ba28cf25e1e
DavidP1996erea <david.perea@iesnervion.es>
2022-11-17 18:23:03 +0100

    Initial commit

---
+++ src/GestorHojas.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorHojas extends Thread {
+
+	private  volatile static List<String> lista = new ArrayList<String>();
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			lista.add(new String("Texto" + i));
+		}
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
+
+		for (String string : lista) {
+			System.out.println(string);
+		}
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			new GestorHojas().start();
+		}
+
+	}
+
+}
